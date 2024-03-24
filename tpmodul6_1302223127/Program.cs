53791415935623a53e80698d09ec4de1df10d88f
Fauzein Mulya Warman <fauzeinmulyawarman@Fauzeins-MacBook-Pro.local>
2024-03-24 20:56:17 +0700

    Menambahkan class SayaTubeVideo

--- tpmodul6_1302223127/Program.cs
+++ tpmodul6_1302223127/Program.cs
@@ -1,7 +1,12 @@
-﻿internal class Program
+﻿using tpmodul6_1302223127;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - Fauzein Mulya Warman");
+
+        video.IncreasePlayCount(100);
+        video.PrintVideoDetail();
     }
 }
---
+++ tpmodul6_1302223127/SayaTubeVideo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace tpmodul6_1302223127
+{
+	public class SayaTubeVideo
+	{
+		private int id;
+		private string title;
+		private int playCount;
+
+		public SayaTubeVideo(string title)
+		{
+			this.title = title;
+			this.id = new Random().Next(11111, 99999);
+			this.playCount = 0;
+		}
+
+		public void IncreasePlayCount(int playCount)
+		{
+			this.playCount += playCount;
+		}
+
+		public void PrintVideoDetail()
+		{
+			Console.WriteLine("=== Detail Video ===");
+			Console.WriteLine($"ID : {this.id}");
+			Console.WriteLine($"Title : {this.title}");
+			Console.WriteLine($"Play Count : {this.playCount}");
+		}
+	}
+}
+
